1e8ff8fadd45a60639dfe212e3e480ae6198493d
Willian <willianmeln@gmail.com>
2024-02-23 20:43:27 -0500

    First Commit

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,36 @@
+import { CORE_CONCEPTS } from './components/CoreConcepts/data.js'
+import Header from './components/Header/Header.jsx'
+import CoreConcept from './components/CoreConcepts/CoreConcept.jsx';
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+        <section>
+          <h2>Examples</h2>
+          <menu>
+            
+          </menu>
+        </section>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
