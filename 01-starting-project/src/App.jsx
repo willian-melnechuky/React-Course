d90cd7043bc75031f8329215f6ac6dae315654ab
Willian <willianmeln@gmail.com>
2024-03-14 21:08:14 -0400

    Learning about React States and conditional components

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,9 +1,33 @@
-import { CORE_CONCEPTS } from './components/CoreConcepts/data.js'
+import { useState } from 'react' //a react hooks - needed to change states
+
+import { CORE_CONCEPTS } from './data.js'
+import { EXAMPLES } from './data.js'
 import Header from './components/Header/Header.jsx'
 import CoreConcept from './components/CoreConcepts/CoreConcept.jsx';
+import Card from './components/Card/Card.jsx';
+import TabButton from './components/TabButton/TabButton.jsx';
 
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState(); // const [ x, y ] this is array destructuring
+ 
+  function handleSelect(selectedButton) {
+    //selected button 
+    setSelectedTopic(selectedButton) ;
+}
+  let tabContent = <p>Please select a topic</p>
+  
+  if(selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
   return (
     <div>
       <Header/>
@@ -21,13 +45,29 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
         </section>
-        <section>
+        <section id="examples">
           <h2>Examples</h2>
           <menu>
-            
+            <TabButton onSelect = {() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect = {() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect = {() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect = {() => handleSelect('state')}>State</TabButton>
           </menu>
+          {/* {!selectedTopic ? 
+            <p>Please select a topic</p> 
+            :(
+              <div id="tab-content">
+                  <h3>{EXAMPLES[selectedTopic].title}</h3>
+                  <p>{EXAMPLES[selectedTopic].description}</p>
+                  <pre>{EXAMPLES[selectedTopic].code}</pre>
+                </div>
+              )
+          } */}
+          {/* <!-- ? means ==! undefined - meaning the value is not undefined--> */}
+        {tabContent}
         </section>
         <h2>Time to get started!</h2>
+        
       </main>
     </div>
   );
