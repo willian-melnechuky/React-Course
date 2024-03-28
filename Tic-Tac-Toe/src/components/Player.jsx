685d63b1060259bf667d8f5eaa9b341d6167aace
Willian <willianmeln@gmail.com>
2024-03-27 23:37:35 -0400

    Designed the Player Boxes - Allowing for player name edit

---
+++ Tic-Tac-Toe/src/components/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+export default function Player ({initialName, symbol}){
+    const [name, setName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+    
+    function handleEditClick(EditState) {
+        setIsEditing(EditState);
+        //setIsEditing(editing => !editing)
+      }
+    
+      function handleChange(event) {
+        //console.log(event)
+        setName(event.target.value);
+      }
+      
+    let PlayerNameBlock = <span className="player-name">{name}</span>
+    let ButtonBlock = <button onClick={() => handleEditClick(true)}>Edit</button>
+    
+    if (isEditing) {
+        PlayerNameBlock = <input className="player-name" required value={name} onChange={handleChange}/> //This is a two way binding
+        ButtonBlock = <button onClick={() => handleEditClick(false)}>Save</button>
+    }
+
+    
+        
+    return(
+        <li>
+            <span className="player">
+                {PlayerNameBlock}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            {ButtonBlock}
+        </li>
+    )
+}
