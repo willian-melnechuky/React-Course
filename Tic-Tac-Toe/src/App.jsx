f9f98b3430519df9404640cd34239b8f1552efc1
Willian <willianmeln@gmail.com>
2024-03-31 14:33:39 -0400

    Added Log component and changed the gameboard state handle to build board base on log information

--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react';
 import Player from "./components/player"
 import GameBoard from "./components/GameBoard"
+import Log from './components/Log';
+
 function App() {
+  const [gameTurns,setGameTurns] = useState([])
   const [activePlayer, setActivePlayer] = useState('X')
 
-  function handleActivePlayer() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X'
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+      const updatedTurns = [{ 
+        square: { row: rowIndex, col: colIndex} 
+        ,player: currentPlayer
+        ,...prevTurns
+      }];
+      return updatedTurns
+    });
   }
   return (
     <main>
@@ -14,8 +29,12 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleActivePlayer} activePlayerSymbol={activePlayer}/>
+        <GameBoard 
+          onSelectSquare={handleSelectSquare} 
+          turns = {gameTurns}
+          />
       </div>
+      <Log/>
     </main>
   )
 }
--- Tic-Tac-Toe/src/components/GameBoard.jsx
+++ Tic-Tac-Toe/src/components/GameBoard.jsx
@@ -1,23 +1,28 @@
-import {useState} from 'react'
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
-
 ]
 
-export default function GameBoard ({ onSelectSquare, activePlayerSymbol }) {
-    const [gameBoard, setGameboard] = useState(initialGameBoard)
-    
-    function handleSelectedSquare(rowIndex,colIndex) {
-        setGameboard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
-        onSelectSquare();
+export default function GameBoard ({ onSelectSquare, turns }) {
+    let gameBoard = initialGameBoard;
+
+    for (const turn of turns) {
+        const {square, player} = turn;
+        const {row, col } = square;
+
+        gameBoard[row][col]=player;
     }
+    // const [gameBoard, setGameboard] = useState(initialGameBoard)
+    
+    // function handleSelectedSquare(rowIndex,colIndex) {
+    //     setGameboard((prevGameBoard) => {
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+    //     onSelectSquare();
+    // }
 
     return (
         <ol id="game-board">
@@ -26,7 +31,7 @@ export default function GameBoard ({ onSelectSquare, activePlayerSymbol }) {
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
                     <li key={colIndex}>
-                        <button onClick={() => handleSelectedSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                        <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                     </li>))}
                 </ol>
             </li>))}
